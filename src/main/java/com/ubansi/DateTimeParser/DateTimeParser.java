87c7734ca33d5481cef350820772e5d10106253f
ubansi <goakafu@gmail.com>
2017-11-29 11:07:02 +0900

    add LocalDate Parse function

--- src/main/java/com/ubansi/DateTimeParser/DateTimeParser.java
+++ src/main/java/com/ubansi/DateTimeParser/DateTimeParser.java
@@ -1,15 +1,18 @@
 package com.ubansi.DateTimeParser;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DateTimeParser {
 
-	private final static List<DateTimeFormatter> LOCAL_DATE_FORMAT = new ArrayList<DateTimeFormatter>() {
+	private final static List<DateTimeFormatter> LOCAL_DATE_FORMAT_LIST = new ArrayList<DateTimeFormatter>() {
 		{
+			add(DateTimeFormatter.ISO_LOCAL_DATE);
 			add(DateTimeFormatter.BASIC_ISO_DATE);
 		}
 	};
@@ -18,7 +21,23 @@ public class DateTimeParser {
 
 	public LocalDateTime parse(String input) {
 
-		return null;
+		LocalDateTime result= null;
+
+		for(DateTimeFormatter formatter: LOCAL_DATE_FORMAT_LIST) {
+			try {
+			LocalDate date = LocalDate.parse(input, formatter);
+			result =  date.atStartOfDay();
+			}catch(DateTimeParseException e) {
+				System.out.println(e.getMessage());
+			}
+
+		}
+
+		if(result== null) {
+			throw new DateTimeParseException("日付の解析に失敗しました。", input, 0);
+		}
+
+		return result;
 	}
 
 	public ZoneOffset getZoneOffset() {
--- src/test/java/com/ubansi/DateTimeParser/DateTimeParserTest.java
+++ src/test/java/com/ubansi/DateTimeParser/DateTimeParserTest.java
@@ -1,18 +1,40 @@
 package com.ubansi.DateTimeParser;
 
+import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 
 import junit.framework.TestCase;
 
 public class DateTimeParserTest extends TestCase {
+	private DateTimeParser dtp = new DateTimeParser();
+	private DateTimeFormatter formater = DateTimeFormatter.ISO_DATE_TIME;
 
 	@Test
 	public void testSetZoneId() {
 		DateTimeParser dtp = new DateTimeParser();
 		dtp.setZoneOffset("+9");
 
-		assertEquals("+09:00",ZoneOffset.of("+9").getId());
+		assertEquals("+09:00", ZoneOffset.of("+9").getId());
+	}
+
+	@Test
+	public void testMySQLFormat() {
+		List<String> mysqlDates = new ArrayList<String>() {
+			{
+				add("2017-12-02");
+			}
+		};
+
+		for (String date : mysqlDates) {
+			LocalDateTime time = dtp.parse(date);
+
+			assertEquals(time.format(formater),"2017-12-02T00:00:00");
+		}
+
 	}
 }
