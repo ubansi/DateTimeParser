33c8c6c14ec2cf6c91ccd1478b9a82f087a27d8e
ubansi <goakafu@gmail.com>
2017-11-30 10:40:26 +0900

    add comment.

--- src/main/java/com/ubansi/DateTimeConverter/parser/DateTimeParserInterface.java
+++ src/main/java/com/ubansi/DateTimeConverter/parser/DateTimeParserInterface.java
@@ -4,5 +4,13 @@ import java.time.LocalDateTime;
 
 public interface DateTimeParserInterface {
 
+	/**
+	 * This function forcibly converts a string to a date.
+	 *<p>
+	 * If the return value is null, please throw an {@code DateTimeParseException}.
+	 * </p>
+	 * @param input
+	 * @return LocalDateTime instance or {@code null}
+	 */
 	public LocalDateTime parse(String input);
 }
